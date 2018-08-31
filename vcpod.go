ddafb4bb878e0bafa79c755ba3b619341a53d291
himu62 <zero.256dic@gmail.com>
2018-09-01 05:41:42 +0900

    https

--- vcpod.go
+++ vcpod.go
@@ -1,12 +1,16 @@
 package main
 
 import (
+	"crypto/tls"
 	"fmt"
 	"net/http"
+
+	"github.com/himu62/vcpod/secure"
 	"github.com/pkg/browser"
 )
 
 type apiHandler struct{}
+
 func (h *apiHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintf(w, "api mock")
 }
@@ -16,11 +20,22 @@ func main() {
 	mux.Handle("/api/", &apiHandler{})
 	mux.Handle("/", http.FileServer(http.Dir("public_html")))
 
+	cert, err := secure.CreateDummyCert()
+	if err != nil {
+		panic(err)
+	}
+
+	srv := &http.Server{
+		Addr:      ":8888",
+		Handler:   mux,
+		TLSConfig: &tls.Config{Certificates: cert},
+	}
+
 	fmt.Println(`Welcome to vcpod!
 
-Open http://localhost:8888/ to main page
+Open https://localhost:8888/ to main page
 Type Ctrl-C to quit`)
 
-	browser.OpenURL("http://localhost:8888/")
-	http.ListenAndServe(":8888", mux)
+	browser.OpenURL("https://localhost:8888/")
+	srv.ListenAndServeTLS("", "")
 }
